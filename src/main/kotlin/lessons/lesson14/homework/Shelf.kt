d1e83d89ace3d43a784ef49c5b34b245009674d2
Dmitriy <arsenal_lion@mail.ru>
2026-03-18 17:58:21 +0300

    added homework for lesson 14

--- src/main/kotlin/lessons/lesson14/homework/Shelf.kt
+++ src/main/kotlin/lessons/lesson14/homework/Shelf.kt
@@ -1,8 +1,33 @@
 package org.example.lessons.lesson14.homework
 
-class Shelf (val capacity: Int) {
-    val items = mutableListOf<String>()
-    fun addItem (item: String):Boolean {
-        
+class Shelf(val capacity: Int, val items: MutableList<String>) {
+
+    fun addItem(item: String): Boolean {
+        if (canAccommodate(item)) {
+            items.add(item)
+            return true
+        } else return false
     }
+
+    fun removeItem(item: String): Boolean {
+        if (items.contains(item)) {
+            items.remove(item)
+            return true
+        } else return false
+    }
+
+    fun canAccommodate(item: String): Boolean {
+        val itemsLegth = items.toString().trim().length
+        if (item.length + itemsLegth <= capacity) {
+            return true
+        } else return false
+    }
+
+    fun containsItem(item: String): Boolean {
+        if (items.contains(item)) {
+            return true
+        } else return false
+    }
+
+    fun getItems() = items.toList()
 }
