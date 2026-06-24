e27a26784ce9c9a83ca194ab77715b3975a311bb
Dmitriy <arsenal_lion@mail.ru>
2026-06-24 10:58:42 +0300

    added corrections for homework for lesson 25

--- src/main/kotlin/lessons/lesson25/homework/Homework.kt
+++ src/main/kotlin/lessons/lesson25/homework/Homework.kt
@@ -1,23 +1,23 @@
 package org.example.lessons.lesson25.homework
 
-fun timeTracker (ex: () -> List<Int>) : Long{
-    System.currentTimeMillis()
+fun timeTracker(ex: () -> List<Int>): Long {
+    val start = System.currentTimeMillis()
     ex()
-    println(System.currentTimeMillis())
-    return System.currentTimeMillis()
+    val stop = System.currentTimeMillis()
+    println(stop - start)
+    return (stop - start)
 }
 
-fun ex3 (person: Person) {
+fun ex3(person: Person) {
     with(person) {
         println("Имя $name, возраст $age, почта $email")
     }
 }
 
-fun Person?.toEmployee (): Employee?{
-    let{
-        Employee(it?.name ?: "", it?.age ?: 0, position = "")
+fun Person?.toEmployee(): Employee? {
+    return this?.let {
+        Employee(name, age, position = "")
     }
-    return toEmployee()
 }
 
 fun main() {
@@ -26,14 +26,14 @@ fun main() {
         list.sorted()
     }
 
-    timeTracker {myFunction()}
+    timeTracker { myFunction() }
 
     val employee1 = Employee("Коля", 31, "Аналитик")
-        .apply { email = "kolya1997@mail.ru"; department = "IT"  }
+        .apply { email = "kolya1997@mail.ru"; department = "IT" }
 
     val person1 = Person("Юля", 30).also { it.email = "julka-pulka@gmail.com"; ex3(it) }
 
-    with (person1) { Employee(name, age, position = "Девопс") }
+    with(person1) { Employee(name, age, position = "Девопс") }
 
     person1.run {
         Employee(name, age, position = "Девопс")
